9ae906bb84299cdcd04c79d7d6c7954c0f7bf517
jorgemhtdev <jorge@jorgemht.dev>
2021-04-25 21:23:23 +0200

    Update INotificationManager.cs

--- localNotificationXF/Services/INotificationManager.cs
+++ localNotificationXF/Services/INotificationManager.cs
@@ -4,9 +4,9 @@
 
     public interface INotificationManager
     {
-        event EventHandler NotificationReceived;
         void Initialize();
         void SendNotification(string title, string subtitle, string message, DateTime? notifyTime = null);
         void ReceiveNotification(string title, string subtitle, string message);
+        event EventHandler NotificationReceived;
     }
 }
